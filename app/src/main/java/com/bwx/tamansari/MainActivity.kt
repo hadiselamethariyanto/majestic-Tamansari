0110b604b4bbbaba7374879615a577120a6b4c68
Muhammad Hadi <muhammad.hariyanto@tix.id>
2022-11-12 15:57:47 +0700

    remove unnecessary code on main activity

--- app/src/main/java/com/bwx/tamansari/MainActivity.kt
+++ app/src/main/java/com/bwx/tamansari/MainActivity.kt
@@ -1,64 +1,17 @@
 package com.bwx.tamansari
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bwx.tamansari.databinding.ActivityMainBinding
-import com.bwx.tamansari.ui.akun.AkunFragment
-import com.bwx.tamansari.ui.home.HomeFragment
-import com.bwx.tamansari.ui.pesan.PesanFragment
-import com.bwx.tamansari.ui.riwayat.RiwayatFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
-    private val mOnNavigationItemSelectedListener =
-        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_riwayat -> {
-                    val fragment = RiwayatFragment()
-                    addFragment(fragment)
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.navigation_home -> {
-                    val fragment = HomeFragment()
-                    addFragment(fragment)
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.navigation_akun -> {
-                    val fragment = AkunFragment()
-                    addFragment(fragment)
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.navigation_pesan -> {
-                    val fragment = PesanFragment()
-                    addFragment(fragment)
-                    return@OnNavigationItemSelectedListener true
-                }
-            }
-            false
-        }
-
-    private fun addFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .setCustomAnimations(
-                R.anim.design_bottom_sheet_slide_in,
-                R.anim.design_bottom_sheet_slide_out
-            )
-            .replace(R.id.content, fragment, fragment.javaClass.simpleName)
-            .commit()
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,17 +34,7 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController)
         binding.bottomNav.setupWithNavController(navController)
-
-
-//        val navView: BottomNavigationView = binding.navView
-//
-//        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-//        val fragment = HomeFragment()
-//        addFragment(fragment)
-
         supportActionBar?.hide()
-
-//        navView.background = null
         binding.bottomNav.menu.getItem(2).isEnabled = false
 
         binding.fab.setOnClickListener {

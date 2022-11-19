1aa7fac1b561d349a74eccc1a15ec6a7c8cf7208
Muhammad Hadi <muhammad.hariyanto@tix.id>
2022-11-19 17:03:50 +0700

    add choose ticket viewmodel

--- app/src/main/java/com/bwx/tamansari/ui/wisata/choose_ticket/ChooseTicketWisataFragment.kt
+++ app/src/main/java/com/bwx/tamansari/ui/wisata/choose_ticket/ChooseTicketWisataFragment.kt
@@ -10,12 +10,14 @@ import com.bwx.tamansari.R
 import com.bwx.tamansari.databinding.FragmentChooseTicketWisataBinding
 import com.bwx.tamansari.model.ChartDomain
 import com.bwx.tamansari.ui.base.BaseFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class ChooseTicketWisataFragment :
     BaseFragment<FragmentChooseTicketWisataBinding>(FragmentChooseTicketWisataBinding::inflate) {
 
-    private val chartTicket = arrayListOf<ChartDomain>()
+    private val viewModel: ChooseTicketWisataViewModel by viewModel()
+    private lateinit var chooseTicketWisataAdapter: ChooseTicketWisataAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,48 +26,41 @@ class ChooseTicketWisataFragment :
             arguments?.getParcelableArrayList<TicketWisataDomain>("tickets") ?: arrayListOf()
         val wisata = arguments?.getParcelable<WisataDomain>("wisata")
 
-        val chooseTicketWisataAdapter = ChooseTicketWisataAdapter(tickets)
+        viewModel.chart.observe(viewLifecycleOwner) {
+            chooseTicketWisataAdapter.updateData(it)
+            if (it.isNotEmpty()) {
+                binding.llBook.visibility = View.VISIBLE
+            } else {
+                binding.llBook.visibility = View.GONE
+            }
+        }
+
+        chooseTicketWisataAdapter = ChooseTicketWisataAdapter(tickets)
         chooseTicketWisataAdapter.setOnItemClickCallback(object :
             ChooseTicketWisataAdapter.OnItemClickCallback {
             override fun onChooseTicket(data: TicketWisataDomain) {
-                chartTicket.add(
-                    ChartDomain(
-                        idProduct = data.id,
-                        productName = data.name,
-                        productPrice = data.price,
-                        total = 1
-                    )
+                val chart = ChartDomain(
+                    idProduct = data.id,
+                    productName = data.name,
+                    productPrice = data.price,
+                    total = 1
                 )
-                chooseTicketWisataAdapter.updateData(chartTicket)
+                viewModel.addChart(chart)
             }
 
             override fun onPlusTicket(data: TicketWisataDomain) {
-                for (x in 0 until chartTicket.size) {
-                    if (chartTicket[x].idProduct == data.id) {
-                        chartTicket[x].total++
-                    }
-                }
-                chooseTicketWisataAdapter.updateData(chartTicket)
+                viewModel.plusChart(data)
             }
 
             override fun onMinusTicket(data: TicketWisataDomain) {
-                for (x in 0 until chartTicket.size) {
-                    if (chartTicket[x].idProduct == data.id) {
-                        val total = chartTicket[x].total
-                        if (total == 1) {
-                            chartTicket.removeAt(x)
-                        } else {
-                            chartTicket[x].total--
-                        }
-                    }
-                }
-                chooseTicketWisataAdapter.updateData(chartTicket)
+                viewModel.deleteChart(data)
             }
 
         })
         binding.rvTicketWisata.adapter = chooseTicketWisataAdapter
 
         binding.btnBook.setOnClickListener {
+            val chartTicket = viewModel.chart.value
             val bundle = bundleOf("wisata" to wisata, "charts" to chartTicket)
             findNavController().navigate(
                 R.id.action_navigation_choose_ticket_wisata_to_navigation_review_transaction_wisata,
---
+++ app/src/main/java/com/bwx/tamansari/ui/wisata/choose_ticket/ChooseTicketWisataViewModel.kt
@@ -0,0 +1,43 @@
+package com.bwx.tamansari.ui.wisata.choose_ticket
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import banyuwangi.digital.core.domain.model.TicketWisataDomain
+import com.bwx.tamansari.model.ChartDomain
+
+class ChooseTicketWisataViewModel : ViewModel() {
+    private val _chart = MutableLiveData<ArrayList<ChartDomain>>()
+    val chart: LiveData<ArrayList<ChartDomain>> get() = _chart
+
+    fun addChart(chart: ChartDomain) {
+        val list = _chart.value ?: arrayListOf()
+        list.add(chart)
+        _chart.value = list
+    }
+
+    fun plusChart(data: TicketWisataDomain){
+        val chartTicket = _chart.value ?: arrayListOf()
+        for (x in 0 until chartTicket.size) {
+            if (chartTicket[x].idProduct == data.id) {
+                chartTicket[x].total++
+            }
+        }
+        _chart.value = chartTicket
+    }
+
+    fun deleteChart(data: TicketWisataDomain) {
+        val chartTicket = _chart.value ?: arrayListOf()
+        for (x in 0 until chartTicket.size) {
+            if (chartTicket[x].idProduct == data.id) {
+                val total = chartTicket[x].total
+                if (total == 1) {
+                    chartTicket.removeAt(x)
+                } else {
+                    chartTicket[x].total--
+                }
+            }
+        }
+        _chart.value = chartTicket
+    }
+}

a734c21574d6d836cede4fe138889094693ebda6
Muhammad Hadi <muhammad.hariyanto@tix.id>
2022-12-01 18:36:50 +0700

    add bottom navigation visibility on my ticket travel package and my failed ticket travel package

--- app/src/main/java/com/bwx/tamansari/MainActivity.kt
+++ app/src/main/java/com/bwx/tamansari/MainActivity.kt
@@ -118,6 +118,14 @@ class MainActivity : AppCompatActivity() {
                     supportActionBar?.show()
                     binding.bottomNav.visibility = View.GONE
                 }
+                R.id.navigation_my_ticket_travel_package->{
+                    supportActionBar?.show()
+                    binding.bottomNav.visibility = View.GONE
+                }
+                R.id.navigation_my_failed_ticket_travel_package->{
+                    supportActionBar?.show()
+                    binding.bottomNav.visibility = View.GONE
+                }
                 else -> {
                     supportActionBar?.show()
                     binding.bottomNav.visibility = View.VISIBLE

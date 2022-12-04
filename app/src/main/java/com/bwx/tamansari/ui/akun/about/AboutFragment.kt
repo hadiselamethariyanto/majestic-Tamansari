10f1e0ee3f67356809336ee11a869639037246bb
Muhammad Hadi <adybeldex@gmail.com>
2022-12-05 00:46:40 +0700

    add close button on about fragment

--- app/src/main/java/com/bwx/tamansari/ui/akun/about/AboutFragment.kt
+++ app/src/main/java/com/bwx/tamansari/ui/akun/about/AboutFragment.kt
@@ -11,6 +11,9 @@ class AboutFragment : BaseBottomSheetDialog<FragmentAboutBinding>(FragmentAboutB
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.imgClose.setOnClickListener {
+            dismiss()
+        }
         binding.tvAbout.text =
             "Tamansari App adalah aplikasi yang memudahkan wisatawan yang berkunjung ke Banyuwangi khususnya Desa Tamansari untuk menemukan dan memesan beragam produk perjalanan, local services, dan layanan sampah. \n\nDengan adanya Tamansari App, wisatawan bisa mendapatkan harga yang lebih murah karena membeli langsung kepada pengelola dan tanpa perantara siapapun karena aplikasi ini dikelola langsung oleh Bumdes Ijen Lestari dibawah naungan Desa Wisata Tamansari.\n\nPihak Bumdes sangat berterimakasih kepada Bank BCA karena melalui program Bakti BCA, akselerasi digitalisasi di Desa Tamansari bisa sangat cepat dan terarah."
     }

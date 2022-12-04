85faec0e9b4313ca6263b93aac36120067b0cb02
Muhammad Hadi <adybeldex@gmail.com>
2022-12-04 18:48:01 +0700

    change login toolbar title and login first on tps3r

--- app/src/main/java/com/bwx/tamansari/ui/tpsr/TpsrFragment.kt
+++ app/src/main/java/com/bwx/tamansari/ui/tpsr/TpsrFragment.kt
@@ -3,11 +3,15 @@ package com.bwx.tamansari.ui.tpsr
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.Observer
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import banyuwangi.digital.core.data.Resource
 import banyuwangi.digital.core.domain.model.TpsrBalanceDomain
+import com.bwx.tamansari.R
 import com.bwx.tamansari.databinding.FragmentTpsrBinding
 import com.bwx.tamansari.ui.base.BaseFragment
+import com.bwx.tamansari.ui.login.LoginFragment
 import com.bwx.tamansari.utils.Utils
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -16,14 +20,38 @@ class TpsrFragment : BaseFragment<FragmentTpsrBinding>(FragmentTpsrBinding::infl
     private val viewModel: TpsrViewModel by viewModel()
     private lateinit var adapter: HistoryTpsrAdapter
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val navController = findNavController()
+
+        val currentBackStackEntry = navController.currentBackStackEntry!!
+        val savedStateHandle = currentBackStackEntry.savedStateHandle
+        savedStateHandle.getLiveData<Boolean>(LoginFragment.LOGIN_SUCCESSFUL)
+            .observe(currentBackStackEntry) { success ->
+                if (!success) {
+                    val startDestination = navController.graph.startDestinationId
+                    val navOptions = NavOptions.Builder()
+                        .setPopUpTo(startDestination, true)
+                        .build()
+                    navController.navigate(startDestination, null, navOptions)
+                }
+            }
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupAdapter()
 
         viewModel.getUser()
-        viewModel.user.observe(viewLifecycleOwner) {
-            it?.email?.let { it1 ->
-                viewModel.getTpsrBalance(it1).observe(viewLifecycleOwner, tpsrBalanceObserver)
+        viewModel.user.observe(viewLifecycleOwner) { firebaseUser ->
+            if (firebaseUser == null) {
+                findNavController().navigate(R.id.loginFragment)
+            } else {
+                firebaseUser.email?.let {
+                    viewModel.getTpsrBalance(it).observe(viewLifecycleOwner, tpsrBalanceObserver)
+                }
             }
         }
     }
@@ -45,7 +73,7 @@ class TpsrFragment : BaseFragment<FragmentTpsrBinding>(FragmentTpsrBinding::infl
                 setLoading(false)
                 val data = res.data
                 val saldo = data?.saldo
-                val history = data?.history?: arrayListOf()
+                val history = data?.history ?: arrayListOf()
                 binding.tvSaldo.text = "IDR ${Utils.thousandSeparator(saldo ?: 0)}"
                 adapter.updateData(history)
             }
@@ -55,14 +83,14 @@ class TpsrFragment : BaseFragment<FragmentTpsrBinding>(FragmentTpsrBinding::infl
         }
     }
 
-    private fun setLoading(isLoading:Boolean){
-        if (isLoading){
+    private fun setLoading(isLoading: Boolean) {
+        if (isLoading) {
             binding.tvSaldo.visibility = View.GONE
             binding.shimmerBalance.visibility = View.VISIBLE
             binding.shimmerHistory.visibility = View.VISIBLE
             binding.shimmerBalance.startShimmer()
             binding.shimmerHistory.startShimmer()
-        }else{
+        } else {
             binding.tvSaldo.visibility = View.VISIBLE
             binding.shimmerBalance.stopShimmer()
             binding.shimmerHistory.stopShimmer()
